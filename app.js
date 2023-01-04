943ae2cc296304642f09f0285eb2f3d9eb01c494
man.sokleaksa2020 <man.sokleaksa2020@gmail.com>
2023-01-04 14:38:35 +0700

    Adding a Battle Log

--- app.js
+++ app.js
@@ -8,14 +8,21 @@ const app = Vue.createApp({
             playerHealth : 100,
             monsterHealth : 100,
             currentRound : 0, 
-            winner : null
+            winner : null,
+            logMessages: [],
         };
     },
     computed : {
         monsterBarStyle (){
+            if (this.monsterHealth < 0) {
+                return {width : '0%'};
+            }
             return {width:this.monsterHealth+'%'};
         },
         playerBarStyle (){
+            if(this.playerHealth < 0 ){
+                return {width: '0%'};
+            }
             return {width:this.playerHealth+'%'};
         },
         mayUseSpacialAttack(){
@@ -44,20 +51,30 @@ const app = Vue.createApp({
         }
     },
     methods : {
+        startGame (){
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.currentRound = 0;
+            this.winner = null;
+            this.logMessages = [];
+        },
         attackMonster(){
             this.currentRound++;
            const attackValue = getRandomValue(5,12);
            this.monsterHealth -= attackValue;
+           this.addLogMessage('player','attack',attackValue);
            this.attackPlayer()
         },
         attackPlayer() {
             const attackValue = getRandomValue(8,15); 
+            this.addLogMessage('monster','attack',attackValue);
            this.playerHealth -= attackValue;
         },
         spacailAttackMonster () { 
             this.currentRound++;
             const attackValue = getRandomValue(10,25);
             this.monsterHealth -= attackValue;
+            this.addLogMessage('player','spacail-attack',attackValue);
             this.attackPlayer();
         },
         healPlayer (){
@@ -68,7 +85,18 @@ const app = Vue.createApp({
             }else{
                 this.playerHealth += healValue;
             }
+            this.addLogMessage('player','heal',healValue);
             this.attackPlayer();   
+        },
+        surrender (){
+            this.winner = "monster";
+        },
+        addLogMessage (who, what, value){
+            this.logMessages.unshift({
+             actionBy:who,
+             actionType:what,
+             actionValue : value, 
+            })
         }
     }
 });
